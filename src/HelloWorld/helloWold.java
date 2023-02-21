19ba6f42a174d44e40f1007baafbaacbc6800584
CBarneron <corentin.barneron@gmail.Com>
2023-02-21 07:26:53 -0800

    commentaire

--- src/HelloWorld/helloWold.java
+++ src/HelloWorld/helloWold.java
@@ -5,12 +5,17 @@ public class helloWold {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello patata");
+		
 	}
 	
 	
 	public static void holamundo(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
+	}
+	public static void bonjourmonde(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bonjour le monde");
 	}
 	
 	
